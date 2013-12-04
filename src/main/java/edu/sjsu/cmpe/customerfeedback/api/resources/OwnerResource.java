797878884c62e587cd1b135e8ed15dafe9016338
Kriti <kriti.mahajan6@gmail.com>
2013-12-04 14:40:17 -0800

    updated APIs

--- src/main/java/edu/sjsu/cmpe/customerfeedback/api/resources/OwnerResource.java
+++ src/main/java/edu/sjsu/cmpe/customerfeedback/api/resources/OwnerResource.java
@@ -2,10 +2,10 @@ package edu.sjsu.cmpe.customerfeedback.api.resources;
 
 import javax.validation.Valid;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
+//import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+//import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -15,7 +15,7 @@ import com.yammer.metrics.annotation.Timed;
 import edu.sjsu.cmpe.customerfeedback.domain.Owner;
 import edu.sjsu.cmpe.customerfeedback.dto.LinkDto;
 import edu.sjsu.cmpe.customerfeedback.dto.LinksDto;
-import edu.sjsu.cmpe.customerfeedback.dto.OwnerDto;
+//import edu.sjsu.cmpe.customerfeedback.dto.OwnerDto;
 import edu.sjsu.cmpe.customerfeedback.repository.OwnerRepositoryInterface;
 
 @Path("/owners")
@@ -37,22 +37,16 @@ public class OwnerResource {
 		int ownerId = savedOwner.getOwnerId();
 		LinksDto links = new LinksDto();
 		links.addLink(new LinkDto("view-owner", "/owners/"+ownerId, "GET"));
-		
-		return Response.status(201).entity(links).build();
-		
+		return Response.status(201).entity(links).build();		
 	}
 	
-	@GET
+	/*@GET
 	@Path("/{ownerId}")
 	@Timed(name = "view-owner")
 	public Response viewOwner(@PathParam("ownerId") int ownerId) {
 		Owner owner = ownerRepository.getOwnerbyOwnerID(ownerId);
 		OwnerDto links = new OwnerDto(owner);
-		links.addLink(new LinkDto("view-products-by-owner", "/owners/"+ownerId+"/products", "GET"));
-		
+		links.addLink(new LinkDto("view-products-by-owner", "/owners/"+ownerId+"/products", "GET"));		
 		return Response.ok().entity(links).build();
-	}
-	
-	
-
+	}*/
 }
---
+++ src/main/java/edu/sjsu/cmpe/customerfeedback/domain/Review.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package edu.sjsu.cmpe.customerfeedback.domain;
+
+/**
+ * @author Rajiv
+ *
+ */
+public class Review {
+	
+	private int productId;
+	private String templateText;
+	private String reviewText;
+	private int reviewId;
+	
+	/**
+	 * @return the reviewId
+	 */
+	public int getReviewId() {
+		return reviewId;
+	}
+	/**
+	 * @param reviewId the reviewId to set
+	 */
+	public void setReviewId(int reviewId) {
+		this.reviewId = reviewId;
+	}
+	/**
+	 * @return the productId
+	 */
+	public int getProductId() {
+		return productId;
+	}
+	/**
+	 * @param productId the productId to set
+	 */
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	/**
+	 * @return the templateText
+	 */
+	public String getTemplateText() {
+		return templateText;
+	}
+	/**
+	 * @param templateText the templateText to set
+	 */
+	public void setTemplateText(String templateText) {
+		this.templateText = templateText;
+	}
+	/**
+	 * @return the reviewText
+	 */
+	public String getReviewText() {
+		return reviewText;
+	}
+	/**
+	 * @param reviewText the reviewText to set
+	 */
+	public void setReviewText(String reviewText) {
+		this.reviewText = reviewText;
+	}
+	
+	
+	
+
+}
