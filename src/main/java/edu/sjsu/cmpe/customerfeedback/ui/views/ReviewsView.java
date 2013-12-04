6b0bffebfe765e55c5b0988d179710572e2d2455
Anil Kumar <anil.akbp@gmail.com>
2013-12-04 15:55:11 -0800

    added mustache files for UI

--- src/main/java/edu/sjsu/cmpe/customerfeedback/ui/views/ReviewsView.java
+++ src/main/java/edu/sjsu/cmpe/customerfeedback/ui/views/ReviewsView.java
@@ -2,11 +2,14 @@ package edu.sjsu.cmpe.customerfeedback.ui.views;
 
 import java.util.List;
 
+import com.yammer.dropwizard.views.View;
+
 import edu.sjsu.cmpe.customerfeedback.domain.Review;
 
-public class ReviewsView {
+public class ReviewsView extends View {
 	private final List<Review> reviews;
 	public ReviewsView(List<Review> reviews) {
+		super("reviews.mustache");
 		this.reviews = reviews;
 	}
 
